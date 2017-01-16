bbece2161fcaf90fcb060f28e0dea19933cd27a4
Jeremy Levy <jeremy@Jeremys-MacBook-Pro.local>
2017-01-15 18:21:04 -0800

    first commit

--- index.js
+++ index.js
@@ -2,42 +2,44 @@
 module.change_code = 1;
 var _ = require('lodash');
 var Alexa = require('alexa-app');
-var skill = new Alexa.app('movieratings');
-var MovieDataHelper = require('./movie_data_helper');
-
-
+var skill = new Alexa.app('snowreport');
+var SnowDataHelper = require('./snow_data_helper');
 
 //custom intents
 skill.launch(function(req, res) {
-  var prompt = 'For movie ratings, tell me a movie title.';
+  var prompt = 'For snow report information, tell me a ski resort.';
   res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
-skill.intent('movieRatingsIntent', {
+skill.intent('snowReportIntent', {
     'slots': {
-      'MOVIETITLES': 'MOVIES'
+      'SKIRESORTS': 'RESORTS'
     },
     'utterances': [
-      '{|movie} {|info|ratings} {|for} {-|MOVIETITLES}'
+      '{|snowfall|snow|snowdepth|powder} {|info|report|reports} {|for|at} {-|SKIRESORTS}'
     ]
   },
   function(req, res) {
-    var movieTitle = req.slot('MOVIETITLES');
-    var reprompt = 'Tell me a movie title to get rating information.';
-    if (_.isEmpty(movieTitle)) {
-      var prompt =
-        'I didn\'t hear a movie title. Tell me an movie title.';
+    var skiResort = req.slot('SKIRESORTS');
+    // console.log(JSON.stringify(req,null, ' '))
+    var reprompt = 'Tell me a ski resort to get snow report information.';
+    if (_.isEmpty(skiResort)) {
+      var prompt = 'I didn\'t hear a resort name. Tell me a resort to get started.';
       res.say(prompt).reprompt(reprompt).shouldEndSession(false);
       return true;
     } else {
-      var movieHelper = new MovieDataHelper();
-      console.log(movieTitle);
-      movieHelper.getMovieRating(movieTitle).then(function(movieData) {
-        console.log("Movie Data is: "+JSON.stringify(movieData));
-        res.say(movieHelper.formatMovieResults(movieData)).send();
+      var snowHelper = new SnowDataHelper();
+      console.log("User asked for... "+skiResort);
+      //transform the user untered resort name into the website format
+      var trasformedSkiResort = snowHelper.transformResortName(skiResort);
+      console.log("Sending "+trasformedSkiResort)
+      snowHelper.getSnowData(trasformedSkiResort).then(function(snowData) {
+        // send the card with the resort name as the title and snow report data
+        res.card(snowHelper.capitalizeFirstLetterResortTitle(skiResort), snowHelper.formatSnowResults(snowData, skiResort));
+        res.say(snowHelper.formatSnowResults(snowData, skiResort)).send();
       }).catch(function(err) {
         console.log(err.statusCode);
-        var prompt = 'I didn\'t have data for a movie title ' + movieTitle;
+        var prompt = 'Sorry, I couldn\'t find data for that resort, tell me another resort name.';
         res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
       });
       return false;
@@ -53,7 +55,7 @@ skill.intent("AMAZON.CancelIntent", {}, cancelIntentFunction);
 skill.intent("AMAZON.StopIntent", {}, cancelIntentFunction);
 
 skill.intent('AMAZON.HelpIntent', function(req, res) {
-  res.say("Test help").shouldEndSession(true).send();
+  res.say("You can ask Tahoe Snow for snowfall information. Give me a ski resort and I will tell you how much snow is there. Try this command, ask Tahoe Snow for Heavenly.").shouldEndSession(true).send();
 });
 
 module.exports = skill;
---
+++ snow_data_helper.js
@@ -0,0 +1,159 @@
+'use strict';
+var _ = require('lodash');
+var requestPromise = require('request-promise');
+var cheerio = require('cheerio');
+
+var ENDPOINT_BASE = "http://www.onthesnow.com/california/";
+var ENDPOINT_PAGE = "/ski-resort.html";
+
+function SnowDataHelper() {
+}
+
+SnowDataHelper.prototype.capitalizeFirstLetterResortTitle = function(resortName) {
+  if(resortName){
+    resortName = resortName.split('');
+    var uppercaseLetter = resortName[0].toUpperCase();
+    resortName.splice(resortName[0], 1, uppercaseLetter)
+    return resortName.join('');
+  }else
+  return resortName;
+};
+
+SnowDataHelper.prototype.transformResortName = function(resortName) {
+  // console.log("in transform");
+  //input will be "tahoe", "heavenly"
+   //output will be tahoe-doner
+   resortName = resortName.toLowerCase();
+  switch(resortName) {
+    case "bear mountain":
+      resortName = "bear-mountain";
+      break;
+    case "bear valley":
+      resortName = "bear-valley";
+      break;
+    case "boreal mountain resort":
+    case "boreal mountain":
+    case "boreal":
+      resortName = "boreal";
+      break;
+    case "dodge ridge":
+    case "dodge":
+      resortName = "dodge-ridge";
+      break;
+    case "donner ski ranch":
+    case "donner ranch":
+    case "donner":
+      resortName = "donner-ski-ranch";
+      break;
+    case "homewood mountain resort":
+    case "homewood mountain":
+    case "homewood":
+      resortName = "homewood-mountain-resort";
+      break;
+    case "june mountain":
+    case "june":
+      resortName = "june-mountain";
+      break;
+    case "mammoth mountain ski area":
+    case "mammoth mountain":
+    case "mammoth":
+      resortName = "mammoth-mountain-ski-area";
+      break;
+    case "mountain high":
+      resortName = "mountain-high";
+      break;
+    case "mt baldy":
+    case "baldy":
+      resortName = "mt-baldy";
+      break;
+    case "mt shasta ski park":
+    case "mt shasta":
+    case "shasta":
+      resortName = "mount-shasta-board-ski-park"
+      break;
+    case "sierra-at-tahoe":
+    case "sierra tahoe":
+    case "sierra":
+      resortName = "sierra-at-tahoe";
+      break;
+    case "ski china peak":
+    case "china peak":
+    case "china":
+      resortName = "ski-china-peak";
+      break;
+    case "snow summit":
+      resortName = "snow-summit";
+      break;
+    case "snow valley":
+      resortName = "snow-valley"
+      break;
+    case "soda springs":
+    case "soda":
+      resortName = "soda-springs";
+      break;
+    case "squaw valley - alpine meadows":
+    case "squaw valley":
+    case "squaw":
+      resortName = "squaw-valley-usa";
+      break;
+    case "sugar bowl resort":
+    case "sugar bowl":
+      resortName = "sugar-bowl-resort";
+      break;
+    case "badger pass":
+    case "badger":
+    case "yosemite ski & snowboard area":
+      resortName = "badger-pass";
+      break;
+    case "tahoe":
+      resortName = "tahoe-donner";
+      break;
+    case "heavenly mountain resort":
+    case "heavenly ski resort":
+    case "heavenly resort":
+    case "heavenly":
+      resortName = "heavenly-mountain-resort";
+      break;
+    case "northstar":
+    case "north star":
+      resortName = "northstar-california";
+      break;
+  }
+  return resortName;
+};
+
+SnowDataHelper.prototype.getSnowData = function(resortName) {
+  var options = {
+    method: 'GET',
+    uri: ENDPOINT_BASE + resortName + ENDPOINT_PAGE,
+    json: true
+  };
+  return requestPromise(options);
+};
+
+SnowDataHelper.prototype.formatSnowResults = function(snowDataHTML, resortName) {
+  var $ = cheerio.load(snowDataHTML);
+  var currentSnow, snowDepth;
+  var json = { currentSnow : "", snowDepth : "" };
+
+  json.resortName = resortName;
+  json.currentSnow = $("#content_pos > div:nth-child(4) > div > div > div > a:nth-child(2) > div > div:nth-child(1) > p.bluetxt.sfa").text();
+  json.snowDepth = $("#content_pos > div:nth-child(4) > div > div > div > a:nth-child(2) > div > div:nth-child(1) > p.bluetxt.sd").text();
+  // console.log(JSON.stringify(json))
+  //check if the resort isn't found
+  if (snowDataHTML) {
+    var template = _.template('${resortName} got ${snowNow} of snow today and has ${totalSnowDepth} of total depth.');
+    return template({
+      resortName: json.resortName,
+      snowNow: json.currentSnow,
+      totalSnowDepth: json.snowDepth
+    });
+  } 
+  else {
+    //no valid resort
+    var template =_.template('Sorry, I don\'t have data for that resort, tell me another resort name.');
+    return template();
+  }
+};
+
+module.exports = SnowDataHelper;
