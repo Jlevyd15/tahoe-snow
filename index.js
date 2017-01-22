152c3884c8b9cb76df318bb295f8e7b0274242ac
Jeremy Levy <jeremy@Jeremys-MacBook-Pro.local>
2017-01-21 22:02:59 -0800

    v2 of skill, improved voice model

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 module.change_code = 1;
+var SkillId = "amzn1.ask.skill.3a560f55-b79e-4c6f-8fd0-3b118affaac6";
 var _ = require('lodash');
 var Alexa = require('alexa-app');
 var skill = new Alexa.app('snowreport');
@@ -16,33 +17,42 @@ skill.intent('snowReportIntent', {
       'SKIRESORTS': 'RESORTS'
     },
     'utterances': [
-      '{|snowfall|snow|snowdepth|powder} {|info|report|reports} {|for|at} {-|SKIRESORTS}'
+      '{|info|information|report|reports|snow|snowfall|amount} {|for|at} {-|SKIRESORTS}'
     ]
   },
   function(req, res) {
-    var skiResort = req.slot('SKIRESORTS');
-    // console.log(JSON.stringify(req,null, ' '))
-    var reprompt = 'Tell me a ski resort to get snow report information.';
-    if (_.isEmpty(skiResort)) {
-      var prompt = 'I didn\'t hear a resort name. Tell me a resort to get started.';
-      res.say(prompt).reprompt(reprompt).shouldEndSession(false);
-      return true;
-    } else {
-      var snowHelper = new SnowDataHelper();
-      console.log("User asked for... "+skiResort);
-      //transform the user untered resort name into the website format
-      var trasformedSkiResort = snowHelper.transformResortName(skiResort);
-      console.log("Sending "+trasformedSkiResort)
-      snowHelper.getSnowData(trasformedSkiResort).then(function(snowData) {
-        // send the card with the resort name as the title and snow report data
-        res.card(snowHelper.capitalizeFirstLetterResortTitle(skiResort), snowHelper.formatSnowResults(snowData, skiResort));
-        res.say(snowHelper.formatSnowResults(snowData, skiResort)).send();
-      }).catch(function(err) {
-        console.log(err.statusCode);
-        var prompt = 'Sorry, I couldn\'t find data for that resort, tell me another resort name.';
-        res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
-      });
-      return false;
+    // console.log(JSON.stringify(req, null, ' '))
+    var appId = req.data.session.application.applicationId;
+    //check SkillId and AppId Match
+    console.log("App Id: "+ appId + " - " + "Skill Id: "+ SkillId);
+    if(SkillId === appId) {
+      var skiResort = req.slot('SKIRESORTS');
+      var reprompt = 'Tell me a ski resort to get snow report information.';
+      if (_.isEmpty(skiResort)) {
+        var prompt = 'I didn\'t hear a resort name. Tell me a resort to get started.';
+        res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+        return true;
+      } else {
+        var snowHelper = new SnowDataHelper();
+        console.log("User asked for... "+skiResort);
+        //transform the user untered resort name into the website format
+        var trasformedSkiResort = snowHelper.transformResortName(skiResort);
+        console.log("Sending "+trasformedSkiResort)
+        snowHelper.getSnowData(trasformedSkiResort).then(function(snowData) {
+          // send the card with the resort name as the title and snow report data
+          res.card(snowHelper.capitalizeFirstLetterResortTitle(skiResort), snowHelper.formatSnowResults(snowData, skiResort));
+          res.say(snowHelper.formatSnowResults(snowData, skiResort)).send();
+        }).catch(function(err) {
+          console.log(err.statusCode);
+          var prompt = 'Sorry, I couldn\'t find data for that resort, tell me another resort name.';
+          res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+        });
+        return false;
+      }
+    } 
+    else {
+      console.log("Bad Request 400");
+      return 400;
     }
   });
 
