cefb4fc8e6ef3b35d2fee8cfe2e9ba337f7fc869
Jeremy Levy <jlevyd@gmail.com>
2019-09-21 15:06:22 -0700

    (update) Bug fixes and refactors
    - fix cheerio selector for snowfall data
    - adds error checking before invoking the speak function with the snowfall data
    - adds readme for development, testing and deployment

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 ## The Tahoe Snow Skill provides daily snowfall and total snow depth for Tahoe-area ski and snowboard resorts.
 
 ## The Tahoe Snow Skill provides daily snowfall and total snow depth for Tahoe-area ski and snowboard resorts.Enable the skill to get started: say “Alexa, ask Tahoe Snow …” Use the skill to check snow conditions for 45+ resorts. Contact Us: Please send us your feedback on ways to improve this skill. [Contact Us](mailto:J@JustLevy.com?Subject=Tahoe%20Snow%20Alexa%20Skill)
+
+
+## Development
+
+`yarn install`
+
+Run this to test the requests & responses from AWS Lambda. Note, this command runs the Lambda locally.
+
+`lambda-local -l index.js -h handler -e ./res/event.json`
+
+
+## Deployment
+
+Either upload the files via zip or copy/paste into the Lambda function editior directly on AWS for small changes
+
+https://console.aws.amazon.com/lambda/home?region=us-east-1#/functions/snowreport?tab=graph
+
+1. index.js
+2. snow_data_helper.js
+3. package.json
+
+
+## Testing
+
+After deployment test the live changes here: 
+
+https://developer.amazon.com/alexa/console/ask/test/amzn1.ask.skill.3a560f55-b79e-4c6f-8fd0-3b118affaac6/development/en_US/
--- index.js
+++ index.js
@@ -1,63 +1,93 @@
 'use strict';
 module.change_code = 1;
+var SkillId = "amzn1.ask.skill.3a560f55-b79e-4c6f-8fd0-3b118affaac6";
 var _ = require('lodash');
 var Alexa = require('alexa-app');
 var skill = new Alexa.app('snowreport');
 var SnowDataHelper = require('./snow_data_helper');
 
-//custom intents
-skill.launch(function(req, res) {
-  var prompt = 'For snow report information, tell me a ski resort.';
-  res.say(prompt).reprompt(prompt).shouldEndSession(false);
+//pre runs beofore any intent handlers
+skill.pre = function (req, res, type) {
+	var appId = req.data.session.application.applicationId;
+	console.log("App Id: " + appId + " - " + "Skill Id: " + SkillId);
+	if (appId != SkillId) {
+		// fail ungracefully
+		res.fail("Invalid applicationId");
+	}
+};
+
+//catch any exceptions and return something useful
+skill.post = function (req, res, type, exception) {
+	if (exception) {
+		// always turn an exception into a successful response
+		res.clear().say("Sorry, an error occured: " + exception).send();
+	}
+};
+
+//handle any errors
+skill.error = function (exception, request, response) {
+	response.say("Sorry, an error occured" + exception);
+};
+
+//launch intent
+skill.launch(function (req, res) {
+	var prompt = 'For snow report information, tell me a ski resort.';
+	res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
 
+//intent to get snow report info.
 skill.intent('snowReportIntent', {
-    'slots': {
-      'SKIRESORTS': 'RESORTS'
-    },
-    'utterances': [
-      '{|snowfall|snow|snowdepth|powder} {|info|report|reports} {|for|at} {-|SKIRESORTS}'
-    ]
-  },
-  function(req, res) {
-    var skiResort = req.slot('SKIRESORTS');
-    // console.log(JSON.stringify(req,null, ' '))
-    var reprompt = 'Tell me a ski resort to get snow report information.';
-    if (_.isEmpty(skiResort)) {
-      var prompt = 'I didn\'t hear a resort name. Tell me a resort to get started.';
-      res.say(prompt).reprompt(reprompt).shouldEndSession(false);
-      return true;
-    } else {
-      var snowHelper = new SnowDataHelper();
-      console.log("User asked for... "+skiResort);
-      //transform the user untered resort name into the website format
-      var trasformedSkiResort = snowHelper.transformResortName(skiResort);
-      console.log("Sending "+trasformedSkiResort)
-      snowHelper.getSnowData(trasformedSkiResort).then(function(snowData) {
-        // send the card with the resort name as the title and snow report data
-        res.card(snowHelper.capitalizeFirstLetterResortTitle(skiResort), snowHelper.formatSnowResults(snowData, skiResort));
-        res.say(snowHelper.formatSnowResults(snowData, skiResort)).send();
-      }).catch(function(err) {
-        console.log(err.statusCode);
-        var prompt = 'Sorry, I couldn\'t find data for that resort, tell me another resort name.';
-        res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
-      });
-      return false;
-    }
-  });
+	'slots': {
+		'SKIRESORTS': 'RESORTS'
+	},
+	'utterances': [
+		'{|info|information|report|reports|snow|snowfall} {|for|at|on|about} {-|SKIRESORTS}'
+	]
+},
+	function (req, res) {
+		// console.log(JSON.stringify(req, null, ' '))
+		var skiResort = req.slot('SKIRESORTS');
+		var reprompt = 'Tell me a ski resort to get snow report information.';
+		if (_.isEmpty(skiResort)) {
+			console.log("slot was empty");
+			var prompt = 'I can\'t find that resort. Tell me a Tahoe ski resort to get started.';
+			res.say(prompt).reprompt(reprompt).shouldEndSession(false);
+			return true;
+		} else {
+			var snowHelper = new SnowDataHelper();
+			console.log("User asked for... " + skiResort);
+
+			// transform the user untered resort name into the website format
+			var trasformedSkiResort = snowHelper.transformResortName(skiResort);
+			console.log("Sending " + trasformedSkiResort)
+
+			snowHelper.getSnowData(trasformedSkiResort).then(function (snowData) {
+				const results = snowHelper.formatSnowResults(snowData, skiResort)
+				// send the card with the resort name as the title and snow report data
+				res.card(snowHelper.capitalizeFirstLetterResortTitle(skiResort), results);
+				// send the user the requested snow report
+				res.say(results).send();
+			}).catch(function (err) {
+				console.log("valid resort name was not found, server returns " + err.statusCode);
+				var prompt = 'Sorry, I couldn\'t find data for that resort, tell me another resort name.';
+				res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+			});
+			return false;
+		}
+	});
 
 //built-in intents
-var cancelIntentFunction = function(request, response) {
-  response.say("Goodbye!").shouldEndSession(true);
+var cancelIntentFunction = function (req, res) {
+	res.say("Goodbye!").shouldEndSession(true);
 };
 
 skill.intent("AMAZON.CancelIntent", {}, cancelIntentFunction);
 skill.intent("AMAZON.StopIntent", {}, cancelIntentFunction);
 
-skill.intent('AMAZON.HelpIntent', function(req, res) {
-  var prompt = "You can ask Tahoe Snow for snowfall information. I can tell you daily snowfall and total snowfall depth. Which Tahoe resort would you like a snow report for?";
-  var reprompt = "Tell me a ski resort to get snow report information.";
-  res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
+skill.intent('AMAZON.HelpIntent', function (req, res) {
+	var prompt = "You can ask Tahoe Snow for snowfall information. I can tell you daily snowfall and total snowfall depth. Which Tahoe resort would you like a snow report for?";
+	var reprompt = "Tell me a ski resort to get snow report information.";
+	res.say(prompt).reprompt(reprompt).shouldEndSession(false).send();
 });
 
 module.exports = skill;
